f9575aa863d3242ce9a5be2103b89cae0dc62da4
proohit <direnc99@gmail.com>
2022-05-18 17:38:51 +0200

    feat(docs): better api docs

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,13 +1,46 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { AnimeDbService } from './animedb/animedb.service';
-import { Anime } from '@find-my-anime/shared/interfaces/AnimeDb';
 import { Provider } from '@find-my-anime/shared/constants/Provider';
+import { Anime } from '@find-my-anime/shared/interfaces/AnimeDb';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { AnimeDbService } from './animedb/animedb.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly animedbService: AnimeDbService) {}
 
   @Get()
+  @ApiOperation({
+    description:
+      'Search anime by filter parameters. Default limits result to 20',
+  })
+  @ApiQuery({
+    name: 'id',
+    type: String,
+    description: 'Id of an anime, as provided by a provider',
+    required: false,
+  })
+  @ApiQuery({
+    name: 'provider',
+    type: String,
+    description: 'Anime Provider',
+    enum: Provider,
+    required: false,
+    example: 'Anilist',
+  })
+  @ApiQuery({
+    name: 'query',
+    type: String,
+    description: 'Search query. Searches by title',
+    required: false,
+    example: 'sword art online',
+  })
+  @ApiQuery({
+    name: 'tags',
+    type: String,
+    description: 'Comma separated list of tags to search for',
+    required: false,
+    example: 'fantasy',
+  })
   async queryAnime(
     @Query('id') id?: string,
     @Query('query') query?: string,
@@ -28,7 +61,9 @@ export class AppController {
       tags && decodeURIComponent(tags).split(','),
     );
   }
-
+  @ApiOperation({
+    description: 'Get all available tags',
+  })
   @Get('/tags')
   async getTags(): Promise<string[]> {
     return this.animedbService.getTags();
