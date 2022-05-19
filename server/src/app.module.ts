1938d38ad4491f8255c73b66209219a2bcdd8f3e
proohit <direnc99@gmail.com>
2022-05-19 11:22:23 +0200

    feat: serve web from server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AnimeDbModule } from './animedb/animedb.module';
 import { AppController } from './app.controller';
 import { AnimeEnricherModule } from './enrichment/anime-enricher.module';
@@ -11,6 +13,9 @@ import { AnimeEnricherModule } from './enrichment/anime-enricher.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env', '.env.dev'],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '..', '..', 'web', 'dist'),
     }),
   ],
   controllers: [AppController],
