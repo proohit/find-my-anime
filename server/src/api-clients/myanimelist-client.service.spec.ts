fc2b156c3cd8ce8f1b3fa365e529ced211d7a064
proohit <direnc99@gmail.com>
2022-06-19 17:37:25 +0200

    test: external fields in myanimelist client

--- server/src/api-clients/myanimelist-client.service.spec.ts
+++ server/src/api-clients/myanimelist-client.service.spec.ts
@@ -1,3 +1,4 @@
+import { MYANIMELIST_API_URL } from '@find-my-anime/shared/constants/urls';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
@@ -9,6 +10,7 @@ jest.mock('rxjs', () => ({
 describe('MyAnimeListClient', () => {
   const givenDescription = 'test';
   let myanimelistClient: MyAnimeListClient;
+  let httpService: HttpService;
   beforeEach(async () => {
     (rxjs.lastValueFrom as jest.Mock).mockReturnValue(
       Promise.resolve({
@@ -33,6 +35,7 @@ describe('MyAnimeListClient', () => {
       ],
     }).compile();
     myanimelistClient = module.get<MyAnimeListClient>(MyAnimeListClient);
+    httpService = module.get(HttpService);
   });
 
   describe('getAnime', () => {
@@ -40,6 +43,21 @@ describe('MyAnimeListClient', () => {
       const results = await myanimelistClient.getAnime('1234');
       expect(results.description).toBeDefined();
       expect(results.description).toEqual(givenDescription);
+    });
+
+    it('should use external fields', async () => {
+      const spy = jest.spyOn(httpService, 'get');
+      await myanimelistClient.getAnime('1234', ['synopsis']);
+      expect(spy).toHaveBeenCalledWith(
+        `${MYANIMELIST_API_URL}/anime/1234?fields=synopsis`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            'X-MAL-CLIENT-ID': 'test',
+          },
+        },
+      );
     });
 
     it('should handle error if AniDB API returned an error', async () => {
