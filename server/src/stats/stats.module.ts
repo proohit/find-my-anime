b0558f9dfb7ffe18c6866d131950ad997c6e48a1
proohit <direnc99@gmail.com>
2022-09-25 18:29:40 +0200

    fix: provide ConfigService in StatsModule

--- server/src/stats/stats.module.ts
+++ server/src/stats/stats.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AnimeDbModule } from '../animedb/animedb.module';
 import { RequestCollectorInterceptor } from './collector.interceptor';
 import { StatsController } from './stats.controller';
@@ -8,7 +9,12 @@ import { TelemetryService } from './telemetry.service';
 @Module({
   imports: [AnimeDbModule],
   controllers: [StatsController],
-  providers: [StatsService, RequestCollectorInterceptor, TelemetryService],
+  providers: [
+    StatsService,
+    RequestCollectorInterceptor,
+    TelemetryService,
+    ConfigService,
+  ],
   exports: [StatsService, RequestCollectorInterceptor, TelemetryService],
 })
 export class StatsModule {}
