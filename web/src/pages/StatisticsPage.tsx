3bdd3fc3790c1d06d1664eaa2294481ffb3e4def
proohit <direnc99@gmail.com>
2022-09-26 23:17:48 +0200

    feat: show telemetry stats

--- web/src/pages/StatisticsPage.tsx
+++ web/src/pages/StatisticsPage.tsx
@@ -9,14 +9,30 @@ import {
   useColorModeValue,
   VStack,
 } from "@chakra-ui/react";
+import { TelemetrySource } from "@find-my-anime/shared/interfaces/AnimeDb";
 import { DbStatistics } from "@find-my-anime/shared/interfaces/DbStatistics";
-import { FC, ReactNode, useEffect, useState } from "react";
+import { FC, ReactNode, useEffect, useMemo, useState } from "react";
 import Api from "../Api";
 
 const StatisticsPage: FC = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [statistics, setStatistics] = useState<DbStatistics | undefined>(
     undefined
+  );
+
+  const apiStats = useMemo(
+    () => ({
+      [TelemetrySource.App]: statistics?.telemetry.filter(
+        (data) => data.source === TelemetrySource.App
+      ),
+      [TelemetrySource.External]: statistics?.telemetry.filter(
+        (data) => data.source === TelemetrySource.External
+      ),
+      [TelemetrySource.Anonymous]: statistics?.telemetry.filter(
+        (data) => data.source === TelemetrySource.Anonymous
+      ),
+    }),
+    [statistics]
   );
 
   useEffect(() => {
@@ -38,22 +54,60 @@ const StatisticsPage: FC = () => {
               </Text>
             </StatisticsSubTopic>
           </StatisticsTopic>
+          <StatisticsTopic heading="App Stats">
+            {apiStats[TelemetrySource.App]?.length! > 0 && (
+              <StatisticsSubTopic
+                heading="Requests by App"
+                description="App requests were made by the web app"
+              >
+                <StatisticsList
+                  items={apiStats[TelemetrySource.App]!.map((data) => ({
+                    key: data.data,
+                    value: `${data.data} (${data.count})`,
+                  }))}
+                />
+              </StatisticsSubTopic>
+            )}
+            {apiStats[TelemetrySource.External]?.length! > 0 && (
+              <StatisticsSubTopic
+                heading="Requests by External"
+                description="External requests were made by some other app"
+              >
+                <StatisticsList
+                  items={apiStats[TelemetrySource.External]!.map((data) => ({
+                    key: data.data,
+                    value: `${data.data} (${data.count})`,
+                  }))}
+                />
+              </StatisticsSubTopic>
+            )}
+            {apiStats[TelemetrySource.Anonymous]?.length! > 0 && (
+              <StatisticsSubTopic
+                heading="Requests by Anonymous"
+                description="Anonymous requests were made by an unknown source"
+              >
+                <StatisticsList
+                  items={apiStats[TelemetrySource.Anonymous]!.map((data) => ({
+                    key: data.data,
+                    value: `${data.data} (${data.count})`,
+                  }))}
+                />
+              </StatisticsSubTopic>
+            )}
+          </StatisticsTopic>
           <StatisticsTopic heading="Anime stats">
             <StatisticsSubTopic heading="Total count of anime:">
               <Text>{statistics.anime.count}</Text>
             </StatisticsSubTopic>
             <StatisticsSubTopic heading="Count of anime by season">
-              <Box borderWidth="1px" borderRadius="lg">
-                <List maxH={["sm", "md"]} overflow="auto">
-                  {Object.entries(statistics.anime.seasons).map(
-                    ([season, count]) => (
-                      <ListItem key={season}>
-                        {season} ({count})
-                      </ListItem>
-                    )
-                  )}
-                </List>
-              </Box>
+              <StatisticsList
+                items={Object.entries(statistics.anime.seasons).map(
+                  ([season, count]) => ({
+                    key: season,
+                    value: `${season} (${count})`,
+                  })
+                )}
+              />
             </StatisticsSubTopic>
           </StatisticsTopic>
           <StatisticsTopic heading="Tags">
@@ -61,17 +115,14 @@ const StatisticsPage: FC = () => {
               <Text>{statistics.tags.count}</Text>
             </StatisticsSubTopic>
             <StatisticsSubTopic heading="Most common tags:">
-              <Box borderWidth="1px" borderRadius="lg">
-                <List maxH={["sm", "md"]} overflow="auto">
-                  {Object.entries(statistics.tags.mostUsedTags).map(
-                    ([tag, count]) => (
-                      <ListItem key={tag}>
-                        {tag} ({count})
-                      </ListItem>
-                    )
-                  )}
-                </List>
-              </Box>
+              <StatisticsList
+                items={Object.entries(statistics.tags.mostUsedTags).map(
+                  ([tag, count]) => ({
+                    key: tag,
+                    value: `${tag} (${count})`,
+                  })
+                )}
+              />
             </StatisticsSubTopic>
           </StatisticsTopic>
         </>
@@ -84,6 +135,7 @@ const StatisticsPage: FC = () => {
 type TopicProps = {
   children: ReactNode;
   heading: string;
+  description?: string;
 };
 
 const StatisticsTopic: FC<TopicProps> = (props) => {
@@ -106,7 +158,11 @@ const StatisticsTopic: FC<TopicProps> = (props) => {
   );
 };
 
-const StatisticsSubTopic: FC<TopicProps> = ({ children, heading }) => {
+const StatisticsSubTopic: FC<TopicProps> = ({
+  children,
+  heading,
+  description,
+}) => {
   return (
     <Box my={2}>
       <Text
@@ -117,7 +173,26 @@ const StatisticsSubTopic: FC<TopicProps> = ({ children, heading }) => {
       >
         {heading}
       </Text>
+      {description && (
+        <Text fontSize={["md"]} textAlign="left" m={2}>
+          {description}
+        </Text>
+      )}
       {children}
+    </Box>
+  );
+};
+
+const StatisticsList: FC<{ items: { key: string; value: string }[] }> = ({
+  items,
+}) => {
+  return (
+    <Box borderWidth="1px" borderRadius="lg">
+      <List maxH={["sm", "md"]} overflow="auto">
+        {items.map((item) => (
+          <ListItem key={item.key}>{item.value}</ListItem>
+        ))}
+      </List>
     </Box>
   );
 };
